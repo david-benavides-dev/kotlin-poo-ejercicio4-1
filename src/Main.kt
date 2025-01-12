d788a4aab9b2691eddfa14d0cbff193e56e6f6de
dbenfon341 <dbenfon341@g.educaand.es>
2025-01-12 22:48:36 +0100

    Fix typo + añadidos colores a rect2 y 3.

--- src/Main.kt
+++ src/Main.kt
@@ -2,23 +2,23 @@ import classes.Rectangulo
 
 fun main() {
 
-    val rectangulo1 = Rectangulo(15.0,10.0)
+    val rectangulo1 = Rectangulo(15.0, 10.0)
 
     println("\u001B[31m** INFORMACIÓN RECTÁNGULO1 **\u001B[0m\n$rectangulo1")
     println("Area >> ${rectangulo1.calcularArea()}")
     println("Perímetro >> ${rectangulo1.calcularPerimetro()}")
     println("")
 
-    val rectangulo2 = Rectangulo(5.0,10.0)
+    val rectangulo2 = Rectangulo(5.0, 10.0)
 
-    println("** INFORMACIÓN RECTÁNGULO2 **\n$rectangulo2")
+    println("\u001B[31m** INFORMACIÓN RECTÁNGULO2 **\u001B[0m\n$rectangulo2")
     println("Area >> ${rectangulo2.calcularArea()}")
     println("Perímetro >> ${rectangulo2.calcularPerimetro()}")
     println("")
 
-    val rectangulo3 = Rectangulo(5.0,10.0)
+    val rectangulo3 = Rectangulo(5.0, 10.0)
 
-    println("** INFORMACIÓN RECTÁNGULO3 **\n$rectangulo3")
+    println("\u001B[31m** INFORMACIÓN RECTÁNGULO3 **\u001B[0m\n$rectangulo3")
     println("Area >> ${rectangulo3.calcularArea()}")
     println("Perímetro >> ${rectangulo3.calcularPerimetro()}")
 
