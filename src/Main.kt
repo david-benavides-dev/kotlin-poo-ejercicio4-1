dc6886a9d99d00ee7de0fb7a68e24f1e2a566bf6
dbenfon341 <dbenfon341@g.educaand.es>
2025-01-29 04:15:31 +0100

    KDoc & cambios menores. + Creada función para validar en el init si altura y base > 0.

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,7 @@ import classes.Rectangulo
 
 fun main() {
 
+    // * Instancia de la clase rectángulo con base 15 y altura 10.
     val rectangulo1 = Rectangulo(15.0, 10.0)
 
     println("\u001B[31m** INFORMACIÓN RECTÁNGULO1 **\u001B[0m\n$rectangulo1")
@@ -9,6 +10,7 @@ fun main() {
     println("Perímetro >> ${rectangulo1.calcularPerimetro()}")
     println("")
 
+    // * Instancia de la clase rectángulo con base 5 y altura 10.
     val rectangulo2 = Rectangulo(5.0, 10.0)
 
     println("\u001B[31m** INFORMACIÓN RECTÁNGULO2 **\u001B[0m\n$rectangulo2")
@@ -16,10 +18,20 @@ fun main() {
     println("Perímetro >> ${rectangulo2.calcularPerimetro()}")
     println("")
 
+    // * Instancia de la clase rectángulo con base 5 y altura 10.
     val rectangulo3 = Rectangulo(5.0, 10.0)
 
     println("\u001B[31m** INFORMACIÓN RECTÁNGULO3 **\u001B[0m\n$rectangulo3")
     println("Area >> ${rectangulo3.calcularArea()}")
     println("Perímetro >> ${rectangulo3.calcularPerimetro()}")
 
+    // * Intento de crear un rectángulo con base negativa y altura 5 para que salte excepción.
+    try {
+        val rectangulo4 = Rectangulo(-1.0,5.0)
+        println("\u001B[31m** INFORMACIÓN RECTÁNGULO4 **\u001B[0m\n$rectangulo4")
+        println("Area >> ${rectangulo4.calcularArea()}")
+        println("Perímetro >> ${rectangulo4.calcularPerimetro()}")
+    } catch (e: IllegalArgumentException) {
+        println("ERROR>> ${e.message}")
+    }
 }
--- src/classes/Rectangulo.kt
+++ src/classes/Rectangulo.kt
@@ -1,26 +1,49 @@
 package classes
 
-class Rectangulo (val base: Double, val altura: Double) {
+/**
+ * Representa un rectángulo definido por su base y altura.
+ *
+ * @property base La base del rectángulo. Debe ser mayor que 0.
+ * @property altura La altura del rectángulo. Debe ser mayor que 0.
+ */
+class Rectangulo (private val base: Double, private val altura: Double) {
 
     init {
-        try {
-            require(base > 0) { "Base tiene que ser mayor que 0" }
-            require(altura > 0) { "Altura tiene que ser mayor que 0" }
-        } catch (e: IllegalArgumentException){
-            println(e.message)
-        }
+        require(validarBaseAltura(base, altura)) { "La base tiene que ser mayor que 0." }
     }
 
+    /**
+     * Valida que la base y la altura sean mayores que 0.
+     *
+     * @param base La longitud de la base.
+     * @param altura La longitud de la altura.
+     * @return `true` si ambos son mayores que 0, `false` en caso contrario.
+     */
+    private fun validarBaseAltura(base: Double, altura: Double): Boolean {
+        return base > 0 && altura > 0
+    }
 
+    /**
+     * Calcula el área del rectángulo.
+     *
+     * @return El área del rectángulo.
+     */
     fun calcularArea(): Double {
         return base * altura
     }
 
-
+    /**
+     * Calcula el perímetro del rectángulo.
+     *
+     * @return El perímetro del rectángulo.
+     */
     fun calcularPerimetro(): Double {
         return 2 * (base * altura)
     }
 
+    /**
+     * Devuelve una representación en cadena del rectángulo.
+     */
     override fun toString(): String {
         return "Base: $base y Altura: $altura"
     }
